1e2c01668945183a1ec24b2930164764a7f2fd75
MaximeBoinet <maximeboinet@live.com>
2016-10-28 15:00:30 +0200

    avancement effet

--- index.php
+++ index.php
@@ -2,9 +2,52 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/base.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portail DSP</title>
+	<link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-	<div id="content">
-	</div>
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-4 col-md-offset-4">
+    			<div id="dspico" class="ico deux"><img class="img-responsive" src="picmg/DSP SERVICES 2015.gif"></div>
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-md-4">
+				<div id="dspico" class="ico un"><img class="img-responsive" src="picmg/DSPnettoyageLOGO.PNG"></div>
+			</div>
+			<div class="col-md-4 col-md-offset-4">
+				<div id="dspico" class="ico trois"><img class="img-responsive" src="picmg/DSPnettoyageLOGO.PNG"></div>
+			</div>
+		</div>
+        <div class="row">
+        	<div id="loader-wrapper">
+            
+	            <div id="loader"></div>
+	            <div class="loader-section section-left"></div>
+	            <div class="loader-section section-right"></div>
+
+        	</div>
+        </div>
+	</div> 
+        <script src="js/bootstrap.min.js"></script>     
+        <script src="js/jquery-3.1.1.min.js"></script>     
+        <script>
+
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip(); 
+        });
+        
+       	 $(window).on('load', function() {
+                $('body').addClass('loaded');
+                           
+            });
+
+        </script>
+        
 </body>
+</html>
